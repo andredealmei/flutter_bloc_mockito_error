3315e5bc33492ea5376184c78deca19009b0e899
André de Almeida <33403972+andredealmei@users.noreply.github.com>
2021-11-29 09:06:50 -0300

    first commit

---
+++ test/bloc_test.dart
@@ -0,0 +1,7 @@
+import 'package:mockito/annotations.dart';
+import 'package:mockito_bloc_error/bloc/counter_bloc.dart';
+
+import 'bloc_test.mocks.dart';
+
+@GenerateMocks([CounterBloc])
+void main() {}
